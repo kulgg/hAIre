85029384f23b4cb8b8289c8ed6e764cc585484fc
kulgg <75735874+kulgg@users.noreply.github.com>
2022-09-22 15:57:55 +0200

    Add dynamic application path

--- src/data/applications.ts
+++ src/data/applications.ts
@@ -1,7 +1,12 @@
+export interface ApplicationData {
+	grammarScore: number;
+}
+
 export interface Application {
 	id: string;
 	position: string;
 	positionId: string;
+	data: ApplicationData;
 }
 
 const applications: Application[] = [
@@ -9,32 +14,55 @@ const applications: Application[] = [
 		id: "1237",
 		position: "Junior Software Engineer",
 		positionId: "12",
+		data: {
+			grammarScore: 80,
+		},
 	},
 	{
 		id: "3712",
 		position: "Junior Software Engineer",
 		positionId: "23",
+		data: {
+			grammarScore: 50,
+		},
 	},
 	{
 		id: "8212",
 		position: "Human Resources",
 		positionId: "22",
+		data: {
+			grammarScore: 30,
+		},
 	},
 	{
 		id: "7412",
 		position: "Machine Learning Engineer",
 		positionId: "31",
+		data: {
+			grammarScore: 90,
+		},
 	},
 	{
 		id: "9289",
 		position: "Machine Learning Engineer",
 		positionId: "31",
+		data: {
+			grammarScore: 60,
+		},
 	},
 	{
 		id: "3712",
 		position: "Senior Software Engineer",
 		positionId: "93",
+		data: {
+			grammarScore: 70,
+		},
 	},
 ];
 
+const getApplicationById = (id: string): Application | undefined => {
+	return applications.find((x) => x.id === id);
+};
+
 export default applications;
+export { getApplicationById };
