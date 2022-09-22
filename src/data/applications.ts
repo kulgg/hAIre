12cc924dfc1218bfba99bf68d1bda0d0d7ca1352
kulgg <75735874+kulgg@users.noreply.github.com>
2022-09-22 14:23:59 +0200

    Add application mock data

---
+++ src/data/applications.ts
@@ -0,0 +1,40 @@
+export interface Application {
+	id: string;
+	position: string;
+	positionId: string;
+}
+
+const applications: Application[] = [
+	{
+		id: "1237",
+		position: "Junior Software Engineer",
+		positionId: "12",
+	},
+	{
+		id: "3712",
+		position: "Junior Software Engineer",
+		positionId: "23",
+	},
+	{
+		id: "8212",
+		position: "Human Resources",
+		positionId: "22",
+	},
+	{
+		id: "7412",
+		position: "Machine Learning Engineer",
+		positionId: "31",
+	},
+	{
+		id: "9289",
+		position: "Machine Learning Engineer",
+		positionId: "31",
+	},
+	{
+		id: "3712",
+		position: "Senior Software Engineer",
+		positionId: "93",
+	},
+];
+
+export default applications;
